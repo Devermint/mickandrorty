38dc9e0c88da8144da6266c2afe9a4f5979bff80
3amHonda <3amhonda@gmail.com>
2025-01-23 15:08:01 +0200

    Wallet connect implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,31 @@
 
 import "@fontsource/jetbrains-mono";
 import { ChakraProvider } from "@chakra-ui/react";
-import NavBar from './components/ui/NavBar';
-import TopBar from './components/ui/TopBar';
+import NavBar from "./components/ui/NavBar";
+import TopBar from "./components/ui/TopBar";
 import "./global.css";
-import { system } from './components/theme';
+import { system } from "./components/theme";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { OKXWallet } from "@okwallet/aptos-wallet-adapter";
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body>
-                <ChakraProvider value={system}>
-                    <TopBar />
-                    {children}
-                    <NavBar />
-                </ChakraProvider>
-            </body>
-        </html>
-    );
+  const wallets = [new PetraWallet(), new OKXWallet()];
+  return (
+    <html lang="en">
+      <body>
+        <AptosWalletAdapterProvider plugins={wallets}>
+          <ChakraProvider value={system}>
+            <TopBar />
+            {children}
+            <NavBar />
+          </ChakraProvider>
+        </AptosWalletAdapterProvider>
+      </body>
+    </html>
+  );
 }
