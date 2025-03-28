e665a4df16c02cc20c0b0cae94bd7c481435dfde
honestdev <honestdev@yahoo.com>
2025-03-28 13:35:48 +0200

    chore: Remove middleware file for HTTPS redirection

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,12 +2,17 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Check if the request is using HTTP
-  if (process.env.NODE_ENV === 'production' && !request.headers.get('x-forwarded-proto')?.includes('https')) {
-    // Create the HTTPS URL from the original request
+  if (
+    process.env.NODE_ENV === 'production' && 
+    request.headers.get('x-forwarded-proto') !== 'https'
+  ) {
     const httpsUrl = 'https://' + request.headers.get('host') + request.nextUrl.pathname + request.nextUrl.search
     return NextResponse.redirect(httpsUrl, 301)
   }
   
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/:path*',
 } 
